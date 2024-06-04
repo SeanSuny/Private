6ad652dca79c57f7f0838ffc8115a232dde09be8
SeanSuny <20199755+SeanSuny@users.noreply.github.com>
2024-06-04 20:19:16 +0800

    Update

--- jd_check_cookie.py
+++ jd_check_cookie.py
@@ -34,6 +34,7 @@ class Check():
         headers = {
             'Cookie': ck,
             'Referer': 'https://home.m.jd.com/myJd/home.action',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2102K2C Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36'
         }
         try:
             time.sleep(random.randint(1,5))
